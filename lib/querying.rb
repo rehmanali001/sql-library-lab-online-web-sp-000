1242e26945cea7b1a559976b8abf2428326307e8
Rehman Ali <rehmanali001@gmail.com>
2019-09-09 21:17:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE books.series_id = 1"
+  "SELECT books.title, books.year FROM books JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors INNER JOIN subgenres on authors.id = subgenres.id"
+  "SELECT authors.name, subgenres.name FROM authors INNER JOIN subgenres ON authors.id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
